0199e3ef34f6ad06055d95a684c72c4bc5dfb26d
Kevin Yu <kevinyu567@gmail.com>
2022-09-27 22:53:04 -0700

    Add social media icons to heading

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,3 +1,7 @@
+import {RiFacebookFill, RiInstagramFill} from 'react-icons/ri';
+import {FaEnvelope} from 'react-icons/fa';
+
+
 export default function Heading() {
     return (
         <section id="heading" className="text-white text-center h-screen flex flex-col items-center justify-center p-5 bg-[url('/bg.svg')] bg-cover bg-center bg-fixed">
@@ -9,7 +13,19 @@ export default function Heading() {
             <p className="mb-2">
                 The ninth iteration of GunnHacks, Gunn High School's annual hackathon organized by United Computations.
             </p>
-            <strong className="text-xl">January 21-22, 2023</strong>
+            <strong className="text-xl mb-4">January 21-22, 2023</strong>
+
+            <div className="flex gap-4 text-2xl">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/GunnHacks">
+                    <RiFacebookFill />
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/gunn.hacks/">
+                    <RiInstagramFill />
+                </a>
+                <a href="mailto:ghshacks@gmail.com">
+                    <FaEnvelope />
+                </a>
+            </div>
         </section>
     )
 }
