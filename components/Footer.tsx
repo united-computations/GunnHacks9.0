f27f1876b5559252c35d2f3a9a6c76dcec69d20b
Kevin Yu <kevinyu567@gmail.com>
2022-09-27 23:26:36 -0700

    Narrow footer row gap on phone

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <div className="flex items-center text-xl gap-8">
                 {/* TODO: Abel doesn't have a `font-medium` variant; is it better without bold? */}
                 <h3 className="font-bold">Previous Years:</h3>
-                <div className="flex flex-wrap gap-6">
+                <div className="flex flex-wrap gap-x-6 gap-y-2">
                     {previousYears.map(year => (
                         <a target="_blank" rel="noopener noreferrer" href={`https://${year}.gunnhacks.com`} key={year}>
                             {year}
