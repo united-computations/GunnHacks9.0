36b24388cba1e538b98a079afc0c9a1dc62a9920
Kevin Yu <kevinyu567@gmail.com>
2022-10-02 18:36:27 -0700

    Add AoPS as sponsor!

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -10,8 +10,9 @@ export default function Sponsors() {
                 Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="/docs/sponsorship_prospectus.pdf">here</a>.
             </p>
 
-            <div className="pt-4 flex flex-wrap items-center justify-center">
-                <Sponsor href="https://www.wolfram.com" src="sponsors/wolfram.svg" alt="Wolfram" />
+            <div className="pt-8 pb-2 flex flex-wrap items-center justify-center">
+                <Sponsor href="https://www.wolfram.com" src="/sponsors/wolfram.svg" alt="Wolfram" />
+                <Sponsor href="https://artofproblemsolving.com/" src="/sponsors/aops.png" alt="AoPS" />
             </div>
         </Section>
     )
