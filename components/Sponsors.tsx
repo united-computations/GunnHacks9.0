168c95702d3eb3965e16f9dad9ff1db7e254884f
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-10-13 19:44:20 -0700

    Add Sticker Mule as sponsor!

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -10,9 +10,10 @@ export default function Sponsors() {
                 Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="/docs/sponsorship_prospectus.pdf">here</a>.
             </p>
 
-            <div className="pt-8 pb-2 flex flex-wrap items-center justify-center">
+            <div className="pt-8 pb-2 flex flex-wrap gap-x-3 gap-y-2 items-center justify-center">
                 <Sponsor href="https://www.wolfram.com" src="/sponsors/wolfram.svg" alt="Wolfram" />
-                <Sponsor href="https://artofproblemsolving.com/" src="/sponsors/aops.png" alt="AoPS" />
+                <Sponsor href="https://artofproblemsolving.com" src="/sponsors/aops.png" alt="AoPS" />
+                <Sponsor href="https://mule.to/p3df" src="/sponsors/sticker-mule.png" alt="Sticker Mule" />
             </div>
         </Section>
     )
