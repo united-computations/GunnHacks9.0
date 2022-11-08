74184b4e1f66b09e83bd675a0ad6bf10c1747204
Kevin Yu <kevinyu567@gmail.com>
2022-11-07 22:42:18 -0800

    Change `#heading` to `#`

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 
     return (
         <header className={'fixed text-white z-20 bg-black/40 top-0 inset-x-0 hidden sm:flex gap-6 items-center px-4 py-2 transition duration-200 backdrop-blur-sm ' + (scroll > 0 ? 'shadow-md' : 'hover:shadow-md')}>
-            <a href="#heading">
+            <a href="#">
                 <img className="w-8 h-8" src="/lambda.png" alt="lambda" />
             </a>
             <a href="#about" className="no-underline">About</a>
