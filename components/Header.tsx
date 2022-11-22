0371eee4e0508ebd8dadbd482ad49e977b3e3b03
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-11-21 21:56:21 -0800

    Decrease space between lambda and navs

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,17 @@ export default function Header() {
     const scroll = useScroll();
 
     return (
-        <header className={'fixed text-white z-20 top-0 inset-x-0 hidden sm:flex gap-6 items-center px-4 py-2 transition duration-300 backdrop-blur-sm ' + (scroll > 0 ? 'bg-black/40 shadow-md' : 'hover:bg-black/40 hover:shadow-md')}>
+        <header className={'fixed text-white z-20 top-0 inset-x-0 hidden sm:flex gap-5 px-4 py-2 transition duration-200 backdrop-blur-sm ' + (scroll > 0 ? 'bg-black/40 shadow-md' : 'hover:bg-black/40 hover:shadow-md')}>
             <a href="#">
                 <img className="w-8 h-8" src="/lambda.png" alt="lambda" />
             </a>
-            <a href="#about" className="no-underline">About</a>
-            <a href="#schedule" className="no-underline">Schedule</a>
-            <a href="#prizes" className="no-underline">Prizes</a>
-            <a href="#faq" className="no-underline">FAQ</a>
-            <a href="#sponsors" className="no-underline">Sponsors</a>
+            <nav className="flex gap-6 items-center">
+                <a href="#about" className="no-underline">About</a>
+                <a href="#schedule" className="no-underline">Schedule</a>
+                <a href="#prizes" className="no-underline">Prizes</a>
+                <a href="#faq" className="no-underline">FAQ</a>
+                <a href="#sponsors" className="no-underline">Sponsors</a>
+            </nav>
         </header>
     )
 }
