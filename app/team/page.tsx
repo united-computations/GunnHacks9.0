e894948d0a889e774de20b0064902d8a61a7e130
Kevin Yu <kevinyu567@gmail.com>
2023-01-14 21:25:41 -0800

    Add team formation form embed

---
+++ app/team/page.tsx
@@ -0,0 +1,12 @@
+export default function Register() {
+    return (
+        <div className="flex justify-center">
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLScNiwXcFUZZNyrJn-NX7fO6p-EXZ8O8NMEcyiv3tNkci600iA/viewform?embedded=true"
+                className="absolute border-none w-full h-full inset-0 mx-auto"
+            >
+                Loading…
+            </iframe>
+        </div>
+    )
+}
