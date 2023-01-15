2b7d0a73e6aa608d9b9addcda34c06b38ae6338a
Kevin Yu <kevinyu567@gmail.com>
2023-01-14 21:35:27 -0800

    Redirect `/guide` to hacker guide

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/guide',
+        destination: 'https://docs.google.com/document/d/1YkaZb08YBlyKaE9JvEWDza2RJIUztic3vLehU8p5SZM/edit?usp=sharing',
+        permanent: true,
+      },
+    ]
   }
 }
 
