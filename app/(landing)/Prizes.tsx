60979afa6e1db0a26b228322288785335c994fa6
Kevin Yu <kevinyu567@gmail.com>
2023-01-18 09:55:46 -0800

    Fix key error

--- app/(landing)/Prizes.tsx
+++ app/(landing)/Prizes.tsx
@@ -50,7 +50,7 @@ export default function Prizes() {
         <Section title="Prizes" id="prizes">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
                 {mainPrizes.map(prize => (
-                    <div>
+                    <div key={prize.name}>
                         <h3 className="text-xl font-bold mb-3">{prize.name}</h3>
                         <ul>
                             {prize.value.map(item => (
