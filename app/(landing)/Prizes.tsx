f7f84b445b61df31168227c271ef865687ef3524
Aidan Gu <asifiazadegu@gmail.com>
2023-01-17 22:32:46 -0800

    Prize Tiers Added
    outline added, removed shadows, and made it look better ig.. :D

--- app/(landing)/Prizes.tsx
+++ app/(landing)/Prizes.tsx
@@ -1,51 +1,90 @@
 import Section from '../../components/Section';
 
-const prizes = [
-    {
-        name: 'Prize Pool',
-        value: [
-            'Adafruit Feather',
-            'Arduino Kit x2',
-            'Bluetooth Speaker',
-            'Echo Dot x2',
-            'Electronics Learning Lab',
-            'Gaming Mouse x2',
-            'Giant Enter Key',
-            'LED Lights',
-            'Magnetic Chess',
-            'Portable Charger',
-            'Power Bank',
-            'Stack Overflow Macropad'
-        ]
-    },
-    {
-        name: 'Sponsored Prizes',
-        value: [
-            'Wolfram Award',
-            'Leading Learners Expansion Packs',
-            'AoPS'
-        ]
-    }
+const mainPrizes = [{
+    name: 'Top 3 Teams', 
+    value: 
+    [
+        'Bluetooth Speaker', 
+        'Gaming Mouse', 
+        'Arduino Kit'
+    ]
+},
+{
+    name: 'Category Awards',
+    value: [
+        'Power Bank',
+        'Echo Dot',
+        'Adafruit Feather'
+    ]
+},
+{
+    name: 'Other Awards',
+    value: [
+        'Enter Key',
+        'LED Lights'
+    ]
+},
+{
+    name: 'Sponsored Awards',
+    value: [
+        'Wolfram Award',
+        'Leading Learners Expansion Packs',
+        'AoPS'
+    ]
+}
+];
+
+const miniEventPrizes = [{
+    name: 'Mini Event Awards',
+    value: [
+        'Cow Plush',
+        'RadioShack Kit',
+        'Magnetic Chess'
+    ]
+}
 ];
 
 export default function Prizes() {
     return (
         <Section title="Prizes" id="prizes">
-            <div className="flex w-full py-8">
-            <div className="w-1/2 p-4 bg-white text-center">
-                    <h3 className="text-xl font-bold mb-3">Prize Pool</h3>
-                    <ul className="grid grid-cols-3 gap-4">
-                        {prizes[0].value.map(item => (
-                            <li className="p-4 bg-white rounded-lg shadow-md" key={item}>{item}</li>
+            <div className="prizes-section grid grid-rows-2 grid-cols-3 gap-3">
+                <div className="prize-section top-3-teams" style={{ gridRow: "1 / 2", gridColumn: "1 / 2" }}>
+                    <h3 className="text-xl font-bold mb-3">Top 3 Teams</h3>
+                    <ul>
+                        {mainPrizes[0].value.map(item => (
+                            <li className="prize-item p-4 bg-white rounded-lg" key={item} style={{ borderRadius: "5px", border: "1px solid #e05c5c", marginBottom: "10px" }}>{item}</li>
                         ))}
                     </ul>
                 </div>
-                <div className="w-1/2 p-4 bg-white text-center">
-                    <h3 className="text-xl font-bold mb-3">Sponsorship Awards</h3>
-                    <p className="text-gray-600 mb-3">All participants will receive Free Wolfram|One Access for 30 days.</p>
-                    <ul className="grid grid-cols-3 gap-4">
-                        {prizes[1].value.map(item => (
-                            <li className="p-4 bg-white rounded-lg shadow-md" key={item}>{item}</li>
+                <div className="prize-section category-awards" style={{ gridRow: "1 / 2", gridColumn: "2 / 3" }}>
+                    <h3 className="text-xl font-bold mb-3">Category Awards</h3>
+                    <ul>
+                        {mainPrizes[1].value.map(item => (
+                            <li className="prize-item p-4 bg-white rounded-lg" key={item} style={{ borderRadius: "5px", border: "1px solid #e05c5c", marginBottom: "10px" }}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="prize-section other-awards" style={{ gridRow: "1 / 2", gridColumn: "3 / 4" }}>
+                    <h3 className="text-xl font-bold mb-3">Other Awards</h3>
+                    <ul>
+                        {mainPrizes[2].value.map(item => (
+                            <li className="prize-item p-4 bg-white rounded-lg" key={item} style={{ borderRadius: "5px", border: "1px solid #e05c5c", marginBottom: "10px" }}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="prize-section sponsored-awards" style={{ gridRow: "2 / 3", gridColumn: "1 / 2" }}>
+                    <h3 className="text-xl font-bold mb-3">Sponsored Awards</h3>
+                    <ul>
+                        {mainPrizes[3].value.map(item => (
+                            <li className="prize-item p-4 bg-white rounded-lg" key={item} style={{ borderRadius: "5px", border: "1px solid #e05c5c", marginBottom: "10px" }}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="prize-section mini-event-awards" style={{ gridRow: "2 / 3", gridColumn: "2 / 3" }}>
+                    <h3 className="text-xl font-bold mb-3">Mini Event Awards</h3>
+                    <ul>
+                        {miniEventPrizes[0].value.map(item => (
+                            <li className="prize-item p-4 bg-white rounded-lg" key={item} style={{ borderRadius: "5px", border: "1px solid #e05c5c", marginBottom: "10px" }}>{item}</li>
                         ))}
                     </ul>
                 </div>
